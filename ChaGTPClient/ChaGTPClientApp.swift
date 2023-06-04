42c46b1c1004007842f5658ecc47514e485e1998
matteo <matdeliperi.md@gmail.com>
2023-06-04 17:37:06 +0200

    Initial Commit

---
+++ ChaGTPClient/ChaGTPClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaGTPClientApp.swift
+//  ChaGTPClient
+//
+//  Created by matteo deliperi on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ChaGTPClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
